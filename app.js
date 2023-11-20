a9e76172c4cf8015b9af2b07db359a6dbbb5e103
Jakub Krkoska <kuberto773@github.com>
2023-11-20 12:10:16 +0100

    Changed order of middlewares

--- app.js
+++ app.js
@@ -11,21 +11,22 @@ const authRoutes = require('./auth');
 const forumRoutes = require('./forum');
 let app = express(); 
 
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.use(forumRoutes);
-app.use(authRoutes);
 app.use(require("express-session")({ 
     secret: "Rusty is a dog", 
     resave: true, 
     name: "sessid",
     store: new MongoStore({ mongoUrl: db.client.s.url })
 })); 
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
-app.use(passport.session());
+app.use(forumRoutes);
+app.use(authRoutes);
 
 var port = process.env.PORT || 3000; 
 app.listen(port, function () { 
